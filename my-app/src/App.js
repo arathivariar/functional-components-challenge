85129c0074557819680129a6d5b542a38be92bc8
Arathi Variar <arathivariar@gmail.com>
2024-04-22 20:44:19 +0000

    Commiting various features for the react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import css from "./App.module.css";
-import NavBarSimple from "./components/NavBarSimple";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarSimple />
+      <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
